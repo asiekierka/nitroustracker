34f9799687d2377d2b55b8330dd43571edc7b8a8
Adrian Siekierka <kontakt@asie.pl>
2026-07-14 20:17:26 +0200

    n3ds: reduce .3dsx size by 15 KiB

---
+++ n3ds/source/ctype_shim.c
@@ -0,0 +1,12 @@
+// This lightweight shim shaves about 15 KiB off the .3dsx build size.
+
+#include <stddef.h>
+#include <wctype.h>
+
+int iswspace(wint_t ch) {
+	return ch == 0x20 || (ch >= 0x09 && ch <= 0x0d);
+}
+
+int isdigit(int ch) {
+	return ch >= '0' && ch <= '9';
+}
