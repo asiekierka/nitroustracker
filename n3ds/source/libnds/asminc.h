c278ee63312a436ea3e57bfc6ef4b77fc12bb8d4
Adrian Siekierka <kontakt@asie.pl>
2026-07-14 20:08:58 +0200

    n3ds: fix build

--- n3ds/source/libnds/asminc.h
+++ n3ds/source/libnds/asminc.h
@@ -8,38 +8,59 @@
 #define LIBNDS_ASMINC_H__
 
 #if !__ASSEMBLER__
-#error "This header file is only for use in assembly files!"
+# error "This header file is only for use in assembly files!"
 #endif
 
-.macro BEGIN_ASM_FUNC name
-    section = text arch = unset.section.\section\().\name\(),
-    "ax", % progbits.global \name.type \name,
-    % function
+// clang-format off
 
-                .if \arch ==
-        thumb.balign 2 .thumb.elseif \arch == arm.balign 4 .arm.elseif \arch ==
-        unset
-            // If the architecture it isn't specified, don't set it in this macro.
-            // Set the aligment to the one that will work in all cases.
-            .balign 4 .else.error "Unknown architecture".endif \name
-    :.endm
+.macro BEGIN_ASM_FUNC name section=text arch=unset
+    .section .\section\().\name\(), "ax", %progbits
+    .global \name
+    .type \name, %function
 
-            // Remember to use this macro if one function falls through the following one.
-            // If not, the linker may choose to remove the second one if it isn't referenced
-            // anywhere else.
-            .macro BEGIN_ASM_FUNC_NO_SECTION name arch =
-        unset.global \name.type \name,
-    % function
+    .if \arch == thumb
+        .balign         2
+        .thumb
+    .elseif \arch == arm
+        .balign         4
+        .arm
+    .elseif \arch == unset
+        // If the architecture it isn't specified, don't set it in this macro.
+        // Set the aligment to the one that will work in all cases.
+        .balign         4
+    .else
+        .error "Unknown architecture"
+    .endif
+\name:
+.endm
 
-                .if \arch ==
-        thumb.balign 2 .thumb.elseif \arch == arm.balign 4 .arm.elseif \arch ==
-        unset
-            // If the architecture it isn't specified, don't set it in this macro.
-            // Set the aligment to the one that will work in all cases.
-            .balign 4 .else.error "Unknown architecture".endif \name :.endm
+// Remember to use this macro if one function falls through the following one.
+// If not, the linker may choose to remove the second one if it isn't referenced
+// anywhere else.
+.macro BEGIN_ASM_FUNC_NO_SECTION name arch=unset
+    .global \name
+    .type \name, %function
 
-#define ICACHE_SIZE 0x2000
-#define DCACHE_SIZE 0x1000
+    .if \arch == thumb
+        .balign         2
+        .thumb
+    .elseif \arch == arm
+        .balign         4
+        .arm
+    .elseif \arch == unset
+        // If the architecture it isn't specified, don't set it in this macro.
+        // Set the aligment to the one that will work in all cases.
+        .balign         4
+    .else
+        .error "Unknown architecture"
+    .endif
+\name:
+.endm
+
+#define ICACHE_SIZE     0x2000
+#define DCACHE_SIZE     0x1000
 #define CACHE_LINE_SIZE 32
+
+// clang-format on
 
 #endif // LIBNDS_ASMINC_H__
